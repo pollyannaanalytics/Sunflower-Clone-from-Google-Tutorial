f0b4c4600c7f7503e1f6e16b22e36eac6113631e
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-20 18:25:20 +0800

    add http interceptor dependency in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.7.2")
     implementation ("com.squareup.retrofit2:converter-gson:2.7.2")
     implementation ("com.squareup.okhttp3:okhttp:3.6.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
 
 fun getAccessKey(): String? {
