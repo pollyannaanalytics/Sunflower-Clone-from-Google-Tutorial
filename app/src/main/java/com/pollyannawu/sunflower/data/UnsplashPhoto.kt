193a2b33ecfa78f25f8179108b7d4351b2148671
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-20 19:42:35 +0800

    revise data type of urls and user in unsplashPhoto

--- app/src/main/java/com/pollyannawu/sunflower/data/UnsplashPhoto.kt
+++ app/src/main/java/com/pollyannawu/sunflower/data/UnsplashPhoto.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class UnsplashPhoto(
     @field:SerializedName("id") val id: String,
-    @field:SerializedName("urls") val urls: String,
-    @field:SerializedName("user") val user: String
+    @field:SerializedName("urls") val urls: UnsplashPhotoUrls,
+    @field:SerializedName("user") val user: UnsplashUser
 )
