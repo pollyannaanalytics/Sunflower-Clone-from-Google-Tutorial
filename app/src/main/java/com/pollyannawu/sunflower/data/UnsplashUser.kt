a7f4de7aae87bedc277bd6588898dccb32aa04d4
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-20 19:36:08 +0800

    create unsplashUser data class

---
+++ app/src/main/java/com/pollyannawu/sunflower/data/UnsplashUser.kt
@@ -0,0 +1,13 @@
+package com.pollyannawu.sunflower.data
+
+import com.google.gson.annotations.SerializedName
+
+data class UnsplashUser(
+    @field:SerializedName("name") val name: String,
+    @field:SerializedName("username")val username: String
+){
+    val attributeUrl: String
+        get() {
+            return "https://unsplash.com/$username?utm_source=sunflower&utm_medium=referral"
+        }
+}
