5f08faf6eaf5c0d280f85d30207b18169d96bd26
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-21 17:08:59 +0800

    create plant data class

---
+++ app/src/main/java/com/pollyannawu/sunflower/data/dataclass/Plant.kt
@@ -0,0 +1,19 @@
+package com.pollyannawu.sunflower.data.dataclass
+
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import java.util.Calendar
+
+data class Plant(
+    @PrimaryKey @ColumnInfo(name = "id") val plantId: String,
+    val name: String,
+    val description: String,
+    val growZoneNumber: Int,
+    val wateringInterval: Int = 7,
+    val imageUrl: String = ""
+){
+    fun shouldBeWatered(since: Calendar, lastWateringDate: Calendar) =
+        since > lastWateringDate.apply { add(Calendar.DAY_OF_YEAR, wateringInterval) }
+
+    override fun toString() = name
+}
