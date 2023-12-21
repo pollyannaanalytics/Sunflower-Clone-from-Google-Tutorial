9d7a4ef5a7a489fb5197dc4f04a3e8590241bb51
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-21 16:16:25 +0800

    set getSearchResultStream function in repository to load data with paging source and return as flow

--- app/src/main/java/com/pollyannawu/sunflower/data/UnsplashRepository.kt
+++ app/src/main/java/com/pollyannawu/sunflower/data/UnsplashRepository.kt
@@ -1,4 +1,22 @@
 package com.pollyannawu.sunflower.data
 
-class UnsplashRepository {
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.pollyannawu.sunflower.network.UnsplashApiService
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UnsplashRepository @Inject constructor(private val service: UnsplashApiService) {
+
+    fun getSearchResultStream(query: String): Flow<PagingData<UnsplashPhoto>>{
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
+            pagingSourceFactory = { UnsplashPagingSource(service, query)}
+        ).flow
+    }
+
+    companion object{
+        private const val NETWORK_PAGE_SIZE = 25
+    }
 }
