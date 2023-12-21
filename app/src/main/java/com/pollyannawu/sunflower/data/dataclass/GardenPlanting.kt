3db3f048e925152c247fe9445d7b7b7c926351aa
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-21 17:13:46 +0800

    set garden_planting's plant_id as foreign key to Plant data class

--- app/src/main/java/com/pollyannawu/sunflower/data/dataclass/GardenPlanting.kt
+++ app/src/main/java/com/pollyannawu/sunflower/data/dataclass/GardenPlanting.kt
@@ -3,14 +3,16 @@ package com.pollyannawu.sunflower.data.dataclass
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.Calendar
 
 @Entity(
     tableName = "garden_plantings",
     foreignKeys = [
-
-    ]
+        ForeignKey(entity = Plant::class, parentColumns = ["id"], childColumns = ["plant_id"])
+    ],
+    indices = [Index("plant_id")]
 )
 data class GardenPlanting(
     @ColumnInfo(name = "plant_id") val plantId: String,
