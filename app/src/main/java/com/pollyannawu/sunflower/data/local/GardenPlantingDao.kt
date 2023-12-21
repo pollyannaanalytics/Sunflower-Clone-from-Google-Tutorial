6359a9f1801aea2c8e2e30a2f6cf69cdca937b52
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-21 17:31:44 +0800

    implement dao method between room and GardenPlanting data class in GardenPlantingDao

--- app/src/main/java/com/pollyannawu/sunflower/data/local/GardenPlantingDao.kt
+++ app/src/main/java/com/pollyannawu/sunflower/data/local/GardenPlantingDao.kt
@@ -1,9 +1,31 @@
 package com.pollyannawu.sunflower.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
+import com.pollyannawu.sunflower.data.dataclass.GardenPlanting
+import com.pollyannawu.sunflower.data.dataclass.PlantAndGardenPlantings
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface GardenPlantingDao {
 
+    @Query("SELECT * FROM garden_plantings")
+    fun getGardenPlantings(): Flow<List<GardenPlanting>>
+
+    @Query("SELECT EXISTS (SELECT 1 FROM garden_plantings WHERE plant_id = :plantId LIMIT 1)")
+    fun isPlanted(plantId: String): Flow<Boolean>
+
+    @Transaction
+    @Query("SELECT * FROM plants WHERE id IN (SELECT DISTINCT (plant_id) FROM garden_plantings)")
+    fun getPlantedGardens(): Flow<List<PlantAndGardenPlantings>>
+
+    @Insert
+    suspend fun insertGardenPlanting(gardenPlanting: GardenPlanting): Long
+
+    @Delete
+    suspend fun deleteGardenPlanting(gardenPlanting: GardenPlanting)
 }
