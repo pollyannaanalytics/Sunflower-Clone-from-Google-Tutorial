c8632ddaa5b83ecebd7350cc5e0bdd5e5a2e85fc
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-22 16:00:33 +0800

    put data logic in GardenPlantingRepository

--- app/src/main/java/com/pollyannawu/sunflower/data/local/GardenPlantingRepository.kt
+++ app/src/main/java/com/pollyannawu/sunflower/data/local/GardenPlantingRepository.kt
@@ -1,4 +1,33 @@
 package com.pollyannawu.sunflower.data.local
 
-class GardenPlantingRepository {
+import com.pollyannawu.sunflower.data.dataclass.GardenPlanting
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GardenPlantingRepository @Inject constructor(private val gardenPlantingDao: GardenPlantingDao){
+    suspend fun createGardenPlanting(plantId: String){
+        val gardenPlanting = GardenPlanting(plantId)
+        gardenPlantingDao.insertGardenPlanting(gardenPlanting)
+    }
+
+
+    suspend fun removeGardenPlanting(gardenPlanting: GardenPlanting){
+        gardenPlantingDao.deleteGardenPlanting(gardenPlanting)
+    }
+
+    fun isPlanted(plantId: String) = gardenPlantingDao.isPlanted(plantId)
+
+    fun getPlantedGardens() = gardenPlantingDao.getPlantedGardens()
+
+    companion object{
+        @Volatile
+        private var instance: GardenPlantingRepository? = null
+
+        fun getInstance(gardenPlantingDao: GardenPlantingDao){
+            instance ?: synchronized(this){
+                instance ?: GardenPlantingRepository(gardenPlantingDao = gardenPlantingDao).also { instance = it }
+            }
+        }
+    }
 }
