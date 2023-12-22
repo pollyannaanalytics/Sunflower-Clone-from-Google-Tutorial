bbb9b59652f3f106a8e076c024a8aa5f5d311e85
Pollyanna Wu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-12-22 13:29:53 +0800

    add work manager dependency in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.databinding:adapters:3.2.0-alpha11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -92,6 +93,10 @@ dependencies {
     val room_version = "2.5.0"
     implementation ("androidx.room:room-runtime:$room_version")
     annotationProcessor ("androidx.room:room-compiler:$room_version")
+
+    // work manager
+    val work_version = "2.8.0"
+    implementation ("androidx.work:work-runtime-ktx:$work_version")
 }
 
 
